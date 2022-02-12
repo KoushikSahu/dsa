7ac4580a9911ce9d8e166820d0b84d9de0244acd
Koushik Sahu <koushiksahu68@gmail.com>
2022-02-12 21:36:55 +0530

    cycle detection algorithms in graphs

---
+++ src/graph/cycle_directed.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define M_PI 3.14159265358979323846
+#define MOD 1000000007
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+typedef long long ll;
+typedef vector<int> vint;
+typedef vector<vint> vvint;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef pair<int, int> ipair;
+typedef pair<ll, ll> llpair;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+/*
+  Author: Koushik Sahu
+  Created: 2022-02-12 19:57 IST
+*/
+
+enum COLORS {WHITE, GRAY, BLACK};
+
+bool detect_cycle(int i, int n, vector<int>* g, int* color){
+  color[i] = GRAY;
+  bool cycle_found = false;
+  for(int v: g[i]){
+    if(color[v] == GRAY){
+      cycle_found = true;
+      break;
+    }else if(color[v] == WHITE){
+      cycle_found |= detect_cycle(v, n, g, color);
+    }
+  }
+  color[i] = BLACK;
+  return cycle_found;
+}
+
+void solve(){
+  int n, m;
+  cin>>n>>m;
+
+  vector<int> g[n];
+  int u, v;
+  for(int i=0; i<m; i++){
+    cin>>u>>v;
+    g[u].push_back(v);
+  }
+
+  bool cycle_found = false;
+  int color[n];
+  fill_n(color, n, WHITE);
+  for(int i=0; i<n; i++){
+    if(color[i] == WHITE){
+      cycle_found |= detect_cycle(i, n, g, color);
+    }
+  }
+  if(cycle_found) cout<<"Cycle is detected\n";
+  else cout<<"No cycle found!";
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int T=1;
+  //cin>>T;
+  while(T--){
+    solve();
+  }
+  return 0;
+}
+
