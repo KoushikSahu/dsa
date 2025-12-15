95e5a9dc51b11c727ea4fe76cbcd047cf1e05c1e
Koushik Sahu <koushiksahu68@gmail.com>
2025-12-15 09:17:48 +0530

    small optimization in merge_intervals

--- src/nc150/cppsols/merge_intervals.cpp
+++ src/nc150/cppsols/merge_intervals.cpp
@@ -53,7 +53,7 @@ void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v..
 class Solution {
 public:
 	vector<vector<int>> merge(vector<vector<int>>& intervals) {
-		sort(intervals.begin(), intervals.end(), [](vector<int> a, vector<int> b) {
+		sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
 			return a[0] < b[0];
 		});
 		vector<vector<int>> ans;
